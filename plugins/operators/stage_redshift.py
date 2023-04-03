2789a22014d995a6f434708854ca423bdb6ef55e
kebochig <44658604+kebochig@users.noreply.github.com>
2023-04-03 02:39:20 +0100

    Update stage_redshift.py

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,43 +1,73 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 
-class DataQualityOperator(BaseOperator):
+class StageToRedshiftOperator(BaseOperator):
     """
-    Operator that runs data checks against the inserted data.
+    Operator that loads any JSON formatted files from S3 to Amazon Redshift.
     """
 
-    ui_color = '#89DA59'
+    ui_color = '#358140'
+    template_fields = ("s3_key",)
+    copy_query = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        {}
+    """
 
     @apply_defaults
     def __init__(self,
-                 conn_id,
-                 target_tables=[],
+                 table,
+                 drop_table,
+                 aws_connection_id,
+                 redshift_connection_id,
+                 create_query,
+                 s3_bucket,
+                 s3_key,
+                 copy_options,
                  *args, **kwargs):
 
-        super(DataQualityOperator, self).__init__(*args, **kwargs)
-        self.conn_id = conn_id
-        self.target_tables = target_tables
-        self.hook = PostgresHook(postgres_conn_id=self.conn_id)
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        self.table = table
+        self.drop_table = drop_table
+        self.aws_connection_id = aws_connection_id
+        self.redshift_connection_id = redshift_connection_id
+        self.create_query = create_query
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.copy_options = copy_options
 
     def execute(self, context):
-        self.log.info('Start of data checks...')
-        for table in self.target_tables:
-            records = self.hook.get_records(
-                "SELECT COUNT(*) FROM {}".format(table))
-            if len(records) < 1 or len(records[0]) < 1:
-                raise ValueError(
-                    "Data quality check failed. \
-                    {} returned no results".format(table))
-            num_records = records[0][0]
+        self.hook = PostgresHook(postgres_conn_id=self.redshift_connection_id)
+        self.aws_instance = AwsHook(aws_conn_id=self.aws_connection_id)
+        credentials = self.aws_instance.get_credentials()
+        
+        rendered_key = self.s3_key.format(**context)
+        s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
 
-            if num_records < 1:
-                self.log.info(
-                    "No records found in table {}!!".format(table))
-                raise ValueError(
-                    "No records found in table {}!".format(table))
-
+        formatted_query = StageToRedshiftOperator.copy_query.format(
+            self.table,
+            s3_path,
+            credentials.access_key,
+            credentials.secret_key,
+            self.copy_options
+        )
+        if self.drop_table:
+            self.log.info('Dropping {} table if it exists...'.format(
+                self.table))
+            self.hook.run("DROP TABLE IF EXISTS {}".format(self.table))
             self.log.info(
-                "Data quality on table {} \
-                check passed with {} records".format(table, num_records))
+                "Table {} has been successfully dropped".format(
+                    self.table))
+        self.log.info(
+            'Creating {} table if it does not exist...'.format(self.table))
+        self.hook.run(self.create_query)
+        self.log.info("Removing data from {}".format(self.table))
+        self.hook.run("DELETE FROM {}".format(self.table))
+        self.log.info('Executing copy query...')
+        self.hook.run(formatted_query)
+        self.log.info("copy query execution complete...")
